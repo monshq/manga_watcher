01e79e7b54b54f20b09bf09a40767d89e69afaac
monshq <monshq@users.noreply.github.com>
2024-01-27 14:00:29 +0400

    set log level to info for prod

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :manga_watcher, MangaWatcherWeb.Endpoint,
 # config :swoosh, local: false
 
 # Do not print debug messages in production
-# config :logger, level: :info
+config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
