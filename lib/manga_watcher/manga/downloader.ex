b6b9340bb402fbcbe2497e571108ea0e40f391a4
monshq <monshq@users.noreply.github.com>
2024-03-16 01:50:31 +0400

    revert replacing tesla with req

--- lib/manga_watcher/manga/downloader.ex
+++ lib/manga_watcher/manga/downloader.ex
@@ -1,8 +1,23 @@
 defmodule MangaWatcher.Manga.Downloader do
-  @spec download(String.t()) :: {:ok, binary} | {:error, atom}
-  def download(url) do
-    {:ok, Req.get!(url, retry: false, http_errors: :raise).body}
-  rescue
-    e -> {:error, e}
+  @spec download(String.t(), String.t()) :: {:ok, binary} | {:error, atom}
+  def download(url, referer \\ "") do
+    tesla_client =
+      Tesla.client([
+        Tesla.Middleware.FollowRedirects,
+        {Tesla.Middleware.Headers, [{"Referer", referer}]}
+        # Tesla.Middleware.Logger
+      ])
+
+    case Tesla.get(tesla_client, url) do
+      {:ok, request} ->
+        if Integer.to_string(request.status) =~ ~r/2\d\d/ do
+          {:ok, request.body}
+        else
+          {:error, "wrong response code: #{request.status}"}
+        end
+
+      error ->
+        error
+    end
   end
 end
