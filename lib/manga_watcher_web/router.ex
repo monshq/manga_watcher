e9ac563d50a54f0ab69abd5908f6a912c98fc040
monshq <monshq@users.noreply.github.com>
2024-04-30 18:01:56 +0400

    generate user model and pages

--- lib/manga_watcher_web/router.ex
+++ lib/manga_watcher_web/router.ex
@@ -1,6 +1,8 @@
 defmodule MangaWatcherWeb.Router do
   use MangaWatcherWeb, :router
 
+  import MangaWatcherWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule MangaWatcherWeb.Router do
     plug :put_root_layout, html: {MangaWatcherWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_user
 
     plug Plug.Static, at: "/images", from: {:app_name, "priv/static/images"}, gzip: false
 
@@ -23,14 +26,22 @@ defmodule MangaWatcherWeb.Router do
   scope "/", MangaWatcherWeb do
     pipe_through :browser
 
-    live "/", MangaLive.Index, :index
-    live "/mangas", MangaLive.Index, :index
-    live "/mangas/new", MangaLive.Index, :new
-    live "/mangas/:id/edit", MangaLive.Index, :edit
+    live_session :current_user,
+      on_mount: [{MangaWatcherWeb.UserAuth, :mount_current_user}] do
+      live "/", MangaLive.Index, :index
+      live "/mangas", MangaLive.Index, :index
+      live "/mangas/new", MangaLive.Index, :new
+      live "/mangas/:id/edit", MangaLive.Index, :edit
 
-    live "/websites", WebsiteLive.Index, :index
-    live "/websites/new", WebsiteLive.Index, :new
-    live "/websites/:id/edit", WebsiteLive.Index, :edit
+      live "/websites", WebsiteLive.Index, :index
+      live "/websites/new", WebsiteLive.Index, :new
+      live "/websites/:id/edit", WebsiteLive.Index, :edit
+
+      live "/users/confirm/:token", UserConfirmationLive, :edit
+      live "/users/confirm", UserConfirmationInstructionsLive, :new
+
+      delete "/users/log_out", UserSessionController, :delete
+    end
 
     import Phoenix.LiveDashboard.Router
     live_dashboard "/dashboard", metrics: MangaWatcherWeb.Telemetry
@@ -58,5 +69,31 @@ defmodule MangaWatcherWeb.Router do
 
   def auth(conn, opts) do
     Plug.BasicAuth.basic_auth(conn, opts)
+  end
+
+  ## Authentication routes
+
+  scope "/", MangaWatcherWeb do
+    pipe_through [:browser, :redirect_if_user_is_authenticated]
+
+    live_session :redirect_if_user_is_authenticated,
+      on_mount: [{MangaWatcherWeb.UserAuth, :redirect_if_user_is_authenticated}] do
+      live "/users/register", UserRegistrationLive, :new
+      live "/users/log_in", UserLoginLive, :new
+      live "/users/reset_password", UserForgotPasswordLive, :new
+      live "/users/reset_password/:token", UserResetPasswordLive, :edit
+    end
+
+    post "/users/log_in", UserSessionController, :create
+  end
+
+  scope "/", MangaWatcherWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{MangaWatcherWeb.UserAuth, :ensure_authenticated}] do
+      live "/users/settings", UserSettingsLive, :edit
+      live "/users/settings/confirm_email/:token", UserSettingsLive, :confirm_email
+    end
   end
 end
